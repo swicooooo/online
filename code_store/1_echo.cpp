27934477a391ea7183f73626e1cbd42bca67409b
swicooooo <150909278+swicooooo@users.noreply.github.com>
2023-12-11 11:14:28 +0000

    change dircto

--- code_store/1_echo.cpp
+++ code_store/1_echo.cpp
@@ -28,16 +28,16 @@ asio::awaitable<void> listener(){
     }
 } 
 
-int main(){
-    try{
-         asio::io_context ctx(1);
-         asio::signal_set signals(ctx,SIGINT,SIGTERM);
-         signals.async_wait([&](auto,auto){ctx.stop();});
-         asio::co_spawn(ctx,listener(),asio::detached);
-         BOOST_LOG_TRIVIAL(info) << "SERVER】:Start...";
-         ctx.run();
-    }
-    catch(const std::exception& e){
-        BOOST_LOG_TRIVIAL(info) << "【SERVER】:Error..." << e.what();
-    }
-}
+// int main(){
+//     try{
+//          asio::io_context ctx(1);
+//          asio::signal_set signals(ctx,SIGINT,SIGTERM);
+//          signals.async_wait([&](auto,auto){ctx.stop();});
+//          asio::co_spawn(ctx,listener(),asio::detached);
+//          BOOST_LOG_TRIVIAL(info) << "SERVER】:Start...";
+//          ctx.run();
+//     }
+//     catch(const std::exception& e){
+//         BOOST_LOG_TRIVIAL(info) << "【SERVER】:Error..." << e.what();
+//     }
+// }
