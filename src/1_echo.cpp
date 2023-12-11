809ce32471ab736b085abaea96cb15b3105e9804
swicooooo <150909278+swicooooo@users.noreply.github.com>
2023-12-11 05:53:30 +0000

    init

--- src/1_echo.cpp
+++ src/1_echo.cpp
@@ -5,7 +5,6 @@
 using boost::asio::ip::tcp;
 using namespace boost;
 
-
 asio::awaitable<void> echo(tcp::socket sock){
     try{
         std::vector<char> buf(1024);
@@ -29,16 +28,16 @@ asio::awaitable<void> listener(){
     }
 } 
 
-// int main(){
-//     try{
-//          asio::io_context ctx(1);
-//          asio::signal_set signals(ctx,SIGINT,SIGTERM);
-//          signals.async_wait([&](auto,auto){ctx.stop();});
-//          asio::co_spawn(ctx,listener(),asio::detached);
-//          BOOST_LOG_TRIVIAL(info) << "SERVER】:Start...";
-//          ctx.run();
-//     }
-//     catch(const std::exception& e){
-//         BOOST_LOG_TRIVIAL(info) << "【SERVER】:Error..." << e.what();
-//     }
-// }
+int main(){
+    try{
+         asio::io_context ctx(1);
+         asio::signal_set signals(ctx,SIGINT,SIGTERM);
+         signals.async_wait([&](auto,auto){ctx.stop();});
+         asio::co_spawn(ctx,listener(),asio::detached);
+         BOOST_LOG_TRIVIAL(info) << "SERVER】:Start...";
+         ctx.run();
+    }
+    catch(const std::exception& e){
+        BOOST_LOG_TRIVIAL(info) << "【SERVER】:Error..." << e.what();
+    }
+}
